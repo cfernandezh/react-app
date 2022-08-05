e0a5ad81b559ce7f2ce92786ed11625d88566ef8
cfernandezh <fernandezherrera345@gmail.com>
2022-08-05 01:04:13 -0500

    feat(action): adding internal action

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,15 +1,15 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import { getInput, setOutput, setFailed } from "@actions/core";
+import github from "@actions/github";
 
 try {
   // throw(new Error("Some error message"));
-  const name = core.getInput("who-to-greet");
+  const name = getInput("who-to-greet");
   console.log("Hello ${name}");
 
   const time = new Date();
-  core.setOutput("time", time.toTimeString());
+  setOutput("time", time.toTimeString());
 
   console.log(JSON.stringify(github, null, "\t"));
 } catch (error) {
-  core.setFailed("dwdw");
+  setFailed("dwdw");
 }
